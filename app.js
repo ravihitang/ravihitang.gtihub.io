de57d58b92deb7a2a38f744380b9d1fcb19ad2ac
ravihitang <ravihitangofficial25@gmail.com>
2022-11-09 23:05:45 +0530

    part 1 fixed

--- app.js
+++ app.js
@@ -5,12 +5,12 @@ const allsections=document.querySelectorAll('.main-content');
 
 
 function Pagetransistions(){
-    for(let i=0;i<sectbtn.length;i++){
-        sectbtn[i].addEventListener('click'), () =>{
-            let currentBtn=document.querySelectorAll('active-btn');
-            currentBtn[0].className = currentBtn[0].className.replace('active-btn','');
-            this.className += ' active-btn'
-        }
+    for(let i = 0; i < sectbtn.length; i++){
+        sectbtn[i].addEventListener('click', function(){
+            let currentBtn= document.querySelectorAll('.active-btn');     
+            currentBtn[0].className = currentBtn[0].className.replace('active-btn', '');
+            this.className += ' active-btn';
+        })
     }
 }
 Pagetransistions();
